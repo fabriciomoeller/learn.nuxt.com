6ba8948c3c665cf571b14feef93526cf96d0296b
fabriciomoeller <fabriciomoeller@gmail.com>
2023-12-03 18:38:09 -0300

    uno.config.ts - Criado short cuts para css

--- pages/index.vue
+++ pages/index.vue
@@ -11,10 +11,20 @@
 </script>
 
 <template>
-  <div>
-    Hello World {{ x }} {{ y }} <br>
-    <button  @click="toggleDarkMode">
-      Toggle Color Mode {{ color.value }}
-    </button>
-  </div>  
+  <div class="grid grid-cols-[1fr_2fr] h-full">
+    <div class="border-r border-base">
+      Content
+      <div class="text-red">
+        Hello World {{ x }} {{ y }} <br>
+        <button  @click="toggleDarkMode">
+          Toggle Color Mode {{ color.value }}
+        </button>
+      </div>  
+    </div>
+    <div>
+      Plaiground  
+    </div>
+
+  </div>
+  
 </template>
--- uno.config.ts
+++ uno.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig, presetUno, presetIcons, presetAttributify } from 'unocss'
 
 export default defineConfig({
+  shortcuts: {
+    'border-base': 'border-gray:10'
+  },
   presets: [
     presetUno(),
     presetIcons(),
