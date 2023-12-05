9a3fd02e27c4858700ea2b55c4d757361970ac30
fabriciomoeller <fabriciomoeller@gmail.com>
2023-12-04 21:48:29 -0300

    Configurações de uno, etc

--- uno.config.ts
+++ uno.config.ts
@@ -1,13 +1,41 @@
-import { defineConfig, presetUno, presetIcons, presetAttributify } from 'unocss'
+import {
+  defineConfig,
+  presetAttributify,
+  presetIcons,
+  presetTypography,
+  presetUno,
+  presetWebFonts,
+  transformerDirectives,
+} from 'unocss'
 
 export default defineConfig({
   shortcuts: {
-    'border-base': 'border-gray:50',
-    'bg-active': 'bg-gray:20',
+    'border-base': 'border-gray-200 dark:border-gray-800',
+    'bg-active': 'bg-gray:10',
+    'bg-faded': 'bg-gray:5',
+    'bg-base': 'bg-white dark:bg-[#020420]',
+  },
+  theme: {
+    colors: {
+      primary: {
+        DEFAULT: '#00DC82',
+      },
+    },
   },
   presets: [
     presetUno(),
     presetIcons(),
     presetAttributify(),
-  ], 
+    presetWebFonts({
+      provider: 'bunny',
+      fonts: {
+        sans: 'DM Sans',
+        mono: 'DM Mono',
+      },
+    }),
+    presetTypography(),
+  ],
+  transformers: [
+    transformerDirectives(),
+  ],
 })
